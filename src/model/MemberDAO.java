2002774109c1066555cb9895f971362b88401c3c
zica1 <zica1@DESKTOP-2UD5RTV>
2019-06-16 01:38:02 +0900

    0616

--- src/model/MemberDAO.java
+++ src/model/MemberDAO.java
@@ -74,6 +74,17 @@ public class MemberDAO {
 	}	
 	public int delete(Member member) {		
 		int ret = -1; // 0 이상이면 해당 아이디가 존재하므로 삭제, -1이하이면 삭제 실패
+		try {
+			int index = searchByID(member);
+			if(index > 0) { // -1이면 검색 실패, 등록 가능함
+				fw = new MemberFileWriter(file);
+				memberList.remove(member);
+				fw.saveMember(memberList);
+				ret = 0;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
 		
 		return ret;
 	}
